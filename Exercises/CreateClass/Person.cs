99d7deb8108df40efa6777eb6e5a22069e38773b
David Schmidt <david.adam.schmidt@gmail.com>
2019-10-10 14:54:29 +0200

    CreateClass: Finish exercise 1

---
+++ Exercises/CreateClass/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CreateClass
+{
+    internal class Person
+    {
+        public enum Genders
+        {
+            Male,
+            Female
+        }
+
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Genders Gender { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name = {Name}, BirthDate = {BirthDate:yyyy-MM-dd}, Gender = {Gender}";
+        }
+    }
+}
--- Exercises/CreateClass/Program.cs
+++ Exercises/CreateClass/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace CreateClass
+﻿using System;
+
+namespace CreateClass
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            var johnny = new Person
+            {
+                Name = "Johnny",
+                BirthDate = new DateTime(1992, 3, 21),
+                Gender = Person.Genders.Male
+            };
+
+            var mary = new Person
+            {
+                Name = "Mary",
+                BirthDate = new DateTime(1995, 6, 5),
+                Gender = Person.Genders.Female
+            };
+
+            var anonymous = new Person();
+
+            Console.WriteLine(johnny);
+            Console.WriteLine(mary);
+            Console.WriteLine(anonymous);
         }
     }
 }
