e8735375954621cd4f088fc1e69585cfea5b363d
David Schmidt <david.adam.schmidt@gmail.com>
2019-10-10 20:09:12 +0200

    SortString: Finish the assignment

--- Exercises/SortString/Program.cs
+++ Exercises/SortString/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace SortString
+﻿using System;
+
+namespace SortString
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            const string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            var words = str.Split(' ');
+            Array.Sort(words);
+            var result = string.Join(" ", words);
+            Console.WriteLine(result);
         }
     }
 }
