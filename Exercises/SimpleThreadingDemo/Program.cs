3991f33cefbc3d67ed91f716299cc255a3edaee3
David Schmidt <david.adam.schmidt@gmail.com>
2019-11-07 10:12:57 +0100

    Add project files for SimpleThreadingDemo exercise

---
+++ Exercises/SimpleThreadingDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleThreadingDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
