ebbcf8833d841f759393a8c9f77be8a62da23538
David Schmidt <david.adam.schmidt@gmail.com>
2019-11-08 12:24:00 +0100

    SimpleThreadingDemo: Add option to use TPL

--- Exercises/SimpleThreadingDemo/Program.cs
+++ Exercises/SimpleThreadingDemo/Program.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SimpleThreadingDemo
 {
     internal class Program
     {
         private static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "tpl")
+            {
+                Console.WriteLine($"using System.Threading.Tasks{Environment.NewLine}");
+
+                CreateTasks();
+            }
+            else
+            {
+                Console.WriteLine($"using System.Threading{Environment.NewLine}");
+
+                CreateThreads();
+            }
+        }
+
+        private static void CreateTasks()
+        {
+            var task1 = Task.Factory.StartNew(Counting);
+            var task2 = Task.Factory.StartNew(Counting);
+
+            Task.WaitAll(task1, task2);
+        }
+
+        private static void CreateThreads()
         {
             var thread1 = new Thread(Counting);
             var thread2 = new Thread(Counting);
